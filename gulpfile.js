2477c5f930ff216e38f59d68b43ad86ef8a382ee
Cedric GESTES <cedric.gestes@gmail.com>
2015-02-20 19:48:01 +0100

    add gulp support

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+
+    source = require('vinyl-source-stream'),
+    watchify = require('watchify'),
+    browserify = require('browserify'),
+
+    path = require('path'),
+
+    index = './scripts/vispy.js',
+    outdir = './dist/',
+    bundle = 'vispy.js',
+    outfile = 'vispy.js';
+
+function rebundle(file) {
+    if (file) {
+        gutil.log('Rebundling,', path.basename(file[0]), 'has changes.');
+    }
+
+    return this.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source(outfile))
+        .pipe(gulp.dest(outdir));
+}
+
+function createBundler(args) {
+    args = args || {};
+    args.standalone = bundle;
+
+    return browserify(index, args);
+}
+
+/*****
+ * Dev task, incrementally rebuilds the output bundle as the the sources change
+ *****/
+gulp.task('dev', function() {
+    watchify.args.standalone = bundle;
+    var bundler = watchify(createBundler(watchify.args));
+
+    bundler.on('update', rebundle);
+
+    return rebundle.call(bundler);
+});
+
+/*****
+ * Build task, builds the output bundle
+ *****/
+gulp.task('build', function () {
+    return rebundle.call(createBundler());
+});
+
+/*****
+ * JSHint task, lints the lib and test *.js files.
+ *****/
+gulp.task('jshint', function () {
+    return gulp.src([
+            './scripts/**/*.js',
+            'gulpfile.js'
+        ])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-summary'));
+});
+
+/*****
+ * Base task
+ *****/
+gulp.task('default', ['jshint', 'build']);
